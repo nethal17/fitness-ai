4a32c3f6e89c791d0f4f3aaffc0678f8e3fce285
nethal17 <it23259102@my.sliit.lk>
2025-07-13 21:40:32 +0530

    Add Gemini api and fitness plan generation

--- convex/users.ts
+++ convex/users.ts
@@ -1,7 +1,6 @@
 import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 
-
 export const syncUser = mutation({
     args: {
         name: v.string(),
@@ -18,4 +17,23 @@ export const syncUser = mutation({
 
         return await ctx.db.insert("users", args);
     },
-})
+});
+
+export const updateUser = mutation({
+    args: {
+      name: v.string(),
+      email: v.string(),
+      clerkId: v.string(),
+      image: v.optional(v.string()),
+    },
+    handler: async (ctx, args) => {
+      const existingUser = await ctx.db
+        .query("users")
+        .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+        .first();
+  
+      if (!existingUser) return;
+  
+      return await ctx.db.patch(existingUser._id, args);
+    },
+});
